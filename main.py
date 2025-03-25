71b20a97ba19e7c29c9338928b603b439bf70040
seb124 <sstull1842@gmail.com>
2025-03-25 13:05:57 -0500

    Fix main

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import engine
 from classes.computer import Computer
 from classes.aiv1 import AIV1
 from classes.aiv2 import AIV2
+from classes.aiv3 import AIV3
 from classes.user import User
 import json
 
-player_mapper = {"User": User, "Computer": Computer, "AIV1": AIV1, "AIV2": AIV2}
+player_mapper = {"User": User, "Computer": Computer, "AIV1": AIV1, "AIV2": AIV2, "AIV3": AIV3}
 
 def main():
     # prompt user for testing or not
